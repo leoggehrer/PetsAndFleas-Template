2012718cca291ae23230717cab281615ca039af0
Gehrer Gerhard <56598504+leoggehrer@users.noreply.github.com>
2024-10-22 19:37:04 +0200

    Fixed CatUnitTest

--- PetsAndFleas.UnitTest/CatUnitTest.cs
+++ PetsAndFleas.UnitTest/CatUnitTest.cs
@@ -2,48 +2,53 @@
 
 namespace PetsAndFleas.UnitTest
 {
-    [TestMethod]
-    public void ItShouldReturnTrueAndIncrementTreesClimbed_GivenFirstClimb()
+    [TestClass()]
+    public class CatUnitTest
     {
-        // Arrange
-        Cat c1 = new Cat();
 
-        // Act
-        bool result = c1.ClimbOnTree();
+        [TestMethod]
+        public void ItShouldReturnTrueAndIncrementTreesClimbed_GivenFirstClimb()
+        {
+            // Arrange
+            Cat c1 = new Cat();
 
-        // Assert
-        Assert.AreEqual(1, c1.TreesClimbed, "Katze sollte auf 1 Baum geklettert sein!");
-        Assert.AreEqual(true, result, "Katze sollte auf 1 Baum geklettert sein (Rückgabewert true erwartet)!");
-    }
+            // Act
+            bool result = c1.ClimbOnTree();
 
-    [TestMethod]
-    public void ItShouldReturnFalseAndNotIncrementTreesClimbed_GivenClimbWhileOnTree()
-    {
-        // Arrange
-        Cat c1 = new Cat();
-        c1.ClimbOnTree(); // Katze klettert auf den ersten Baum
+            // Assert
+            Assert.AreEqual(1, c1.TreesClimbed, "Katze sollte auf 1 Baum geklettert sein!");
+            Assert.AreEqual(true, result, "Katze sollte auf 1 Baum geklettert sein (Rückgabewert true erwartet)!");
+        }
 
-        // Act
-        bool result = c1.ClimbOnTree(); // Zweiter Versuch, während sie auf dem Baum ist
+        [TestMethod]
+        public void ItShouldReturnFalseAndNotIncrementTreesClimbed_GivenClimbWhileOnTree()
+        {
+            // Arrange
+            Cat c1 = new Cat();
+            c1.ClimbOnTree(); // Katze klettert auf den ersten Baum
 
-        // Assert
-        Assert.AreEqual(1, c1.TreesClimbed, "Katze ist gerade auf einen Baum! Muss vorher runter....");
-        Assert.AreEqual(false, result, "Katze ist gerade auf einen Baum! Rückgabewert false erwartet.");
-    }
+            // Act
+            bool result = c1.ClimbOnTree(); // Zweiter Versuch, während sie auf dem Baum ist
 
-    [TestMethod]
-    public void ItShouldReturnTrueAndIncrementTreesClimbed_GivenClimbAfterClimbingDown()
-    {
-        // Arrange
-        Cat c1 = new Cat();
-        c1.ClimbOnTree(); // Katze klettert auf den ersten Baum
-        c1.ClimbDown(); // Katze klettert wieder herunter
+            // Assert
+            Assert.AreEqual(1, c1.TreesClimbed, "Katze ist gerade auf einen Baum! Muss vorher runter....");
+            Assert.AreEqual(false, result, "Katze ist gerade auf einen Baum! Rückgabewert false erwartet.");
+        }
 
-        // Act
-        bool result = c1.ClimbOnTree(); // Klettert auf den zweiten Baum
+        [TestMethod]
+        public void ItShouldReturnTrueAndIncrementTreesClimbed_GivenClimbAfterClimbingDown()
+        {
+            // Arrange
+            Cat c1 = new Cat();
+            c1.ClimbOnTree(); // Katze klettert auf den ersten Baum
+            c1.ClimbDown(); // Katze klettert wieder herunter
 
-        // Assert
-        Assert.AreEqual(2, c1.TreesClimbed, "Katze sollte auf 2 Bäume geklettert sein!");
-        Assert.AreEqual(true, result, "Katze sollte auf den zweiten Baum geklettert sein (Rückgabewert true erwartet)!");
+            // Act
+            bool result = c1.ClimbOnTree(); // Klettert auf den zweiten Baum
+
+            // Assert
+            Assert.AreEqual(2, c1.TreesClimbed, "Katze sollte auf 2 Bäume geklettert sein!");
+            Assert.AreEqual(true, result, "Katze sollte auf den zweiten Baum geklettert sein (Rückgabewert true erwartet)!");
+        }
     }
 }
